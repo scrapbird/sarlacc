427e84a5a042ddfcb39c599d5544e15a57c51622
Brenton Morris <brenton.smorris@gmail.com>
2017-11-29 23:46:01 +1300

    importer done

--- smtpd/src/mailslurper_import.py
+++ smtpd/src/mailslurper_import.py
@@ -7,41 +7,47 @@ import psycopg2
 import re
 from base64 import b64encode, b64decode
 from configparser import ConfigParser
+import sys
+import asyncio
 
 import storage
 
 
-def cleanupAddress(addr):
+def cleanup_address(addr):
     return addr[1:len(addr)-1]
 
-config = ConfigParser()
-config.read("./smtpd.cfg")
+
+async def main():
+    config = ConfigParser()
+    config.read("./smtpd.cfg")
 
 
-store = storage.StorageControl(config)
+    store = storage.StorageControl(config)
 
-cnx = mysql.connector.connect(
-        user="root", password="root",
-        host="localhost",
-        database="sarlacc")
+    cnx = mysql.connector.connect(
+            user="root", password="root",
+            host="localhost",
+            database="sarlacc")
 
-mysql_cursor = cnx.cursor()
+    mysql_cursor = cnx.cursor()
 
-mysql_cursor.execute("SELECT dateSent, fromAddress, toAddressList, subject, body FROM mailitem;")
+    mysql_cursor.execute("SELECT dateSent, fromAddress, toAddressList, subject, body FROM mailitem;")
 
-for (dateSent, fromAddress, toAddressList, subject, body) in mysql_cursor:
-    # tidy up fromAddress
-    fromAddress = cleanupAddress(re.findall(r"<(.*?)>", fromAddress)[0])
-    print("fromAddress: {}".format(fromAddress))
+    for (dateSent, fromAddress, toAddressList, subject, body) in mysql_cursor:
+        # tidy up fromAddress
+        fromAddress = cleanupAddress(re.findall(r"<(.*?)>", fromAddress)[0])
 
-    # tidy up toaAdressList
-    toAddressList = re.findall(r"<(.*?)>", toAddressList)
-    print("toAddressList: {}".format(toAddressList))
+        # tidy up toaAdressList
+        toAddressList = re.findall(r"<(.*?)>", toAddressList)
 
-    body = b64decode(body)
+        body = str(b64decode(body))
 
-    store.store_email(subject, toAddressList, fromAddress, body, dateSent, [])
+        store.store_email(subject, toAddressList, fromAddress, body, dateSent, [])
 
-mysql_cursor.close()
-cnx.close()
+    mysql_cursor.close()
+    cnx.close()
 
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+loop.close()
